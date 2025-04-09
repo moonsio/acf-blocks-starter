469b0f0dd698fe20fbbb0423867ab794fdb45641
dannymoons <danny@hulpmetmarketing.nl>
2025-04-09 13:19:52 +0200

    feat: init starter

---
+++ moonsio-acf-blocks-starter.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Plugin Name: Moonsio ACF Blocks Starter
+ * Plugin URI: https://moonsio.nl
+ * Description: A starter plugin for ACF blocks using the moonsio theme block registration system
+ * Version: 0.1.0
+ * Author: Moonsio
+ * Author URI: https://moonsio.nl
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: moonsio-acf-blocks
+ * Domain Path: /languages
+ */
+
+defined('ABSPATH') || exit('Forbidden');
+
+use Moonsio\Blocks\BlockRegistry;
+use Moonsio\Fields\FieldTypes;
+
+/**
+ * Check if theme and ACF dependencies exist
+ */
+function moonsio_blocks_check_dependencies()
+{
+  if (!class_exists('ACF')) {
+    add_action('admin_notices', function () {
+      echo '<div class="error"><p>' .
+        esc_html__('The Moonsio ACF Blocks Starter plugin requires Advanced Custom Fields to be installed and activated.', 'moonsio-blocks') .
+        '</p></div>';
+    });
+    return false;
+  }
+
+  if (!class_exists('\Moonsio\Fields\FieldTypes')) {
+    add_action('admin_notices', function () {
+      echo '<div class="error"><p>' .
+        esc_html__('The Moonsio ACF Blocks Starter plugin requires the Moonsio theme to be installed and activated.', 'moonsio-blocks') .
+        '</p></div>';
+    });
+    return false;
+  }
+
+  if (!class_exists('\Moonsio\Blocks\BlockRegistry')) {
+    add_action('admin_notices', function () {
+      echo '<div class="error"><p>' .
+        esc_html__('The Moonsio ACF Blocks Starter plugin requires the Moonsio theme with BlockRegistry to be installed and activated.', 'moonsio-blocks') .
+        '</p></div>';
+    });
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Get all block directories from the plugin
+ * 
+ * @return array Array of block directory names
+ */
+function moonsio_blocks_get_blocks()
+{
+  $blocks_dir = plugin_dir_path(__FILE__) . 'blocks/';
+
+  if (!is_dir($blocks_dir)) {
+    return [];
+  }
+
+  // Get all directory names and filter out . and ..
+  $all_files = scandir($blocks_dir);
+  return array_filter($all_files, function ($item) use ($blocks_dir) {
+    return is_dir($blocks_dir . $item) && $item !== '.' && $item !== '..';
+  });
+}
+
+/**
+ * Register all plugin blocks
+ */
+function moonsio_blocks_register_blocks()
+{
+  if (!moonsio_blocks_check_dependencies()) {
+    return;
+  }
+
+  $plugin_path = plugin_dir_path(__FILE__);
+  $plugin_url = plugin_dir_url(__FILE__);
+
+  // Find all blocks in the blocks directory
+  $blocks = moonsio_blocks_get_blocks();
+
+  if (empty($blocks)) {
+    return;
+  }
+
+  // Get or create BlockRegistry instance
+  global $moonsio_block_registry;
+
+  if (!($moonsio_block_registry instanceof BlockRegistry)) {
+    $moonsio_block_registry = new BlockRegistry();
+  }
+
+  // Register all blocks with the registry
+  foreach ($blocks as $block_name) {
+    $moonsio_block_registry->register_plugin_block(
+      $block_name,
+      $plugin_path,
+      $plugin_url,
+      'moonsio'
+    );
+  }
+}
+
+// Initialize the plugin using ACF's init hook (before the registry runs)
+add_action('acf/init', 'moonsio_blocks_register_blocks', 1);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'blocks/starter-block/style': './assets/scss/blocks/starter-block/style.scss',
+        'blocks/starter-block/editor': './assets/scss/blocks/starter-block/editor.scss',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+}; 
