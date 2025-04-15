946b0caabda3663d938b1a138cf4f4d498c4bbb0
dannymoons <danny@hulpmetmarketing.nl>
2025-04-15 14:03:16 +0200

    fix: gh action

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'moonsio/acf-blocks-starter',
+        'pretty_version' => '0.4.1',
+        'version' => '0.4.1.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'moonsio/acf-blocks-starter' => array(
+            'pretty_version' => '0.4.1',
+            'version' => '0.4.1.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
